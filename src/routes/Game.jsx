978c6ea428300e6a4c4447db1f97a697078e3294
kaylem williams <kaywil56>
2023-05-15 13:16:38 +1200

    basic hand deal

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,5 +1,35 @@
-const Game = () => {
-    return <h1>This is the game page</h1>
-}
+import { useState } from "react";
 
-export default Game
+const Game = () => {
+  const HAND_LENGTH = 5;
+
+  const [hand, setHand] = useState(initializeDeck());
+
+  function initializeDeck() {
+    const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    const deck = [];
+
+    for (let i = 0; i < HAND_LENGTH; i++) {
+      deck.push({
+        suit: suits[Math.floor(Math.random() * suits.length)],
+        value: values[Math.floor(Math.random() * values.length)],
+      });
+    }
+
+    return deck;
+  }
+
+  return (
+    <div>
+      {hand.map((card, idx) => {
+        return (
+          <span style={{ margin: "10px" }} key={`card-${idx}`}>{`${card.value} of ${card.suit}`}</span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Game;
