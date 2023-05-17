925eb2ccdbccdc2e3c7aa4ed3f4a321bd0016a46
kaylem williams <kaywil56>
2023-05-18 09:03:02 +1200

    deal a player a radom hand

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,16 +1,69 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "../AuthContext";
 
 const Game = () => {
   const { authContext } = useContext(AuthContext);
+  const [deck, setDeck] = useState(createDeck);
+  const [hand, setHand] = useState([]);
+
+  function createDeck() {
+    const suits = ["H", "D", "C", "S"];
+    const ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+
+    const deck = [];
+
+    for (const suit of suits) {
+      for (const rank of ranks) {
+        deck.push(rank + suit);
+      }
+    }
+
+    return deck;
+  }
+
+  const dealHand = () => {
+    const newHand = [];
+    for (let i = 0; i < 5; i++) {
+      const randomIndex = Math.floor(Math.random() * deck.length);
+      const randomCard = deck.splice(randomIndex, 1)[0];
+      newHand.push(randomCard);
+    }
+
+    setHand(newHand);
+    setDeck(deck);
+  };
+
+  useEffect(() => {
+    dealHand();
+  }, [deck]);
 
   // Protect route
   if (!authContext.uid) {
     return <Navigate to="/" />;
   }
 
-  return (<div>Game Screen</div>);
+  return (
+    <div>
+      {hand.map((card, idx) => {
+        return <span style={{ margin: "10px" }} key={`card-${idx}`}>{card}</span>;
+      })}
+    </div>
+  );
 };
 
 export default Game;
