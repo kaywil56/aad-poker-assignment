095fc5184ae8d69a4d4e2f4cc2c8bad89c720ef3
kaylem williams <kaywil56>
2023-06-10 13:18:18 +1200

    login and register styles

--- src/routes/LoginRegister.jsx
+++ src/routes/LoginRegister.jsx
@@ -6,6 +6,7 @@ import {
 import { Link } from "react-router-dom";
 import firestore from "../../firestore";
 import { useState } from "react";
+import "./LoginRegister.css";
 
 const LoginRegister = ({ text }) => {
   const auth = getAuth();
@@ -36,32 +37,42 @@ const LoginRegister = ({ text }) => {
   };
 
   return (
-    <>
-      <h1>{text}</h1>
-      <form onSubmit={handleLoginRegister} action="submit">
-        <input
-          onChange={updateCredentials}
-          type="text"
-          placeholder="email"
-          name="email"
-          value={userCredentials.email}
-        />
-        <input
-          onChange={updateCredentials}
-          type="text"
-          placeholder="password"
-          name="password"
-          value={userCredentials.password}
-        />
-        <button type="submit">{text}</button>
-      </form>
-      {text == "Login" && (
-        <>
-          <p>Dont have an account?</p>
-          <Link to="/register">Click here to register</Link>
-        </>
-      )}
-    </>
+    <div className="login-register-container">
+      <div>
+        <form
+          className="login-register-form"
+          onSubmit={handleLoginRegister}
+          action="submit"
+        >
+          <h1 className="login-register-title">{text}</h1>
+          <input
+            className="login-register-inputs"
+            onChange={updateCredentials}
+            type="text"
+            placeholder="email"
+            name="email"
+            value={userCredentials.email}
+          />
+          <input
+            className="login-register-inputs"
+            onChange={updateCredentials}
+            type="text"
+            placeholder="password"
+            name="password"
+            value={userCredentials.password}
+          />
+          <button className="login-register-submit-button" type="submit">
+            {text}
+          </button>
+        </form>
+        {text == "Login" && (
+          <>
+            <p className="login-register-helper-text">Dont have an account?</p>
+            <Link to="/register">Click here to register</Link>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
